77301ce4e9add42588054e877f442f573150c882
ericlan <lanzhuoqin@gmail.com>
2018-03-12 23:21:18 -0400

    Broadcast a message to connected users when someone connects or disconnects

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
+io.on('connection', function (socket) {
+    const ip = socket.request.connection.remoteAddress;
+    io.emit('chat message', ip + ' is connected');
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', ip + '-> ' + msg);
+    });
+
+    socket.on('disconnect', function (data) {
+        io.emit('chat message', ip + ' is disconnected');
     });
 });
 
